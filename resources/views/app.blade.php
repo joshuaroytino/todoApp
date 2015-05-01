aea1b2181080eb3570760ab648e70508267ba510
joshuaroytino <joshuaroytino@gmail.com>
2015-05-01 13:32:11 +0800

    Create app.js and update master view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -59,5 +59,6 @@
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
+    <script src="js/app.js"></script>
 </body>
 </html>
